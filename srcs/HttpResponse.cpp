c426ba1453945e064c856a6760e5eafc11917cda
DoDesCadeaux <dduraku@student.s19.be>
2023-12-17 01:08:28 +0100

    fix bad response request line

--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -1,11 +1,12 @@
 #include "../includes/HttpResponse.hpp"
 
 std::string HttpResponse::getResponse(int statuscode, const std::string &statusmessage, const std::string &bodycontent) {
-	std::string statusline = "HTTP 1.1 " + std::to_string(statuscode) + " " + statusmessage + "\r\n";
+	std::string statusline = "HTTP/1.1 " + std::to_string(statuscode) + " " + statusmessage + "\r\n";
 	std::map<std::string, std::string> headers;
 
 	headers["Content-Type"] = "text/html";
 	headers["Content-Length"] = std::to_string(bodycontent.size());
+	headers["Connection"] = "close";
 
 	std::string response = statusline;
 	for (std::map<std::string, std::string>::iterator it = headers.begin(); it != headers.end(); ++it) {
